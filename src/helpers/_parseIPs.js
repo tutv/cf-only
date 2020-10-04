3807868d805f1048f491c01b7a4f2dd5c9d42d9d
barry <barrydevp@gmail.com>
2020-10-04 18:52:51 +0700

    INIT DRAFT CORE

--- src/helpers/_parseIPs.js
+++ src/helpers/_parseIPs.js
@@ -2,7 +2,7 @@ module.exports = (str) => {
     if (!str || typeof str !== 'string') return []
 
     return str.split('\n')
-        .map(str => str.trim())
+        .map(str => (str + '').trim())
         .filter(Boolean)
 }
 
